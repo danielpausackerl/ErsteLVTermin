b38bb1a86bc166e2ed665b8269c864a9c85b8d47
Daniel Pausackerl <daniel.pausackerl@edu.campus02.at>
2015-10-29 00:02:00 +0100

    Zwischenspeicherung

---
+++ src/Demo.java
@@ -0,0 +1,24 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean treeFront = true;
+		int numberLeaves = 0;
+		char meinZeichen = 'X';
+		int a = 10;
+		int b = 4;
+
+		// System.out.println("Ist ein Baum voraus? " + treeFront);
+
+		// System.out.println(0);
+
+		// System.out.println("Anzahl Blätter: " + numberLeaves);
+		// System.out.println(meinZeichen);
+		numberLeaves = numberLeaves + 1;
+		System.out.println(a + " + " + b + " = " + (a + b));
+
+	}
+
+}
