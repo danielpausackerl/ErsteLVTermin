57dda13a620731a821b141e4b28b58ef7c7357dc
Daniel Pausackerl <daniel.pausackerl@edu.campus02.at>
2015-11-04 20:43:42 +0100

    verbessert

--- src/Demo.java
+++ src/Demo.java
@@ -11,17 +11,36 @@ public class Demo {
 		int b = 5;
 
 		// System.out.println("Ist ein Baum voraus? " + treeFront);
-
 		// System.out.println(0);
-
 		// System.out.println("Anzahl Blätter: " + numberLeaves);
 		// System.out.println(meinZeichen);
-		numberLeaves = numberLeaves + 1;
+		// numberLeaves = numberLeaves + 1;
+
+		System.out.println("a=" + a + ", b=" + b);
+
 		System.out.println(a + " + " + b + " = " + (a + b));
 		System.out.println(a + " - " + b + " = " + (a - b));
 		System.out.println(a + " * " + b + " = " + (a * b));
 		System.out.println(a + " / " + b + " = " + (a / b));
 		System.out.println(a + " % " + b + " = " + (a % b));
+
+		// Leerzeile als Platzhalter
+		System.out.println();
+
+		// Variante 2: Verwende eine weitere Variable namens
+		// result vom Typ int zum Zwischenspeichern der Ergebnisse.
+
+		System.out.println("a=" + a + ", b=" + b);
+		int result = a + b;
+		System.out.println(a + " + " + b + " = " + result);
+		result = a - b;
+		System.out.println(a + " - " + b + " = " + result);
+		result = a * b;
+		System.out.println(a + " * " + b + " = " + result);
+		result = a / b;
+		System.out.println(a + " / " + b + " = " + result);
+		result = a % b;
+		System.out.println(a + " % " + b + " = " + result);
 
 	}
 
